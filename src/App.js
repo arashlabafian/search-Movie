4e8e7178d67b0c75d264fb5b14fb49e5fddef5ea
ArashL <arashlabafian@gmail.com>
2021-10-12 13:45:43 +0330

    add api and search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import "./styles.css";
+import Filter from "./components/Filter/Filter";
+import MoviesList from "./components/MoviesList/MoviesList";
+import api from "./services/api";
 
-function App() {
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const apiKey = "14809d4f97b0780835970cce918adc25";
+
+  const handleFilterChange = useCallback(function (newQuery) {
+    newQuery = {
+      input: newQuery,
+      requestBy: "trending",
+    };
+
+    if (newQuery.input === "") {
+      newQuery = {
+        ...newQuery,
+        input: null,
+        requestBy: "trending",
+        timeWindow: "week",
+      };
+    } else if (newQuery.input !== "") {
+      newQuery = { ...newQuery, requestBy: "search", timeWindow: null };
+    }
+
+    searchMovie(newQuery);
+  }, []);
+
+  async function searchMovie({ requestBy, timeWindow, input }) {
+    const tw = timeWindow ? `/${timeWindow}` : "";
+    api
+      .get(`${requestBy}/movie${tw}?api_key=${apiKey}&query=${input}`)
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter movies={movies} setFilterStateCallback={handleFilterChange} />
+      <main className="container-main">
+        <MoviesList movies={movies} />
+      </main>
     </div>
   );
 }
-
-export default App;
